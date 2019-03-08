06c56e737dfb34f91717ca3ca79e959e418a5eb4
Loic Detienne <lodetien@e-r5-p7.s19.be>
2019-03-08 11:52:52 +0100

    plop

--- roger-deploy.sh
+++ roger-deploy.sh
@@ -9,8 +9,7 @@ if [ $# -eq 1 ]; then
     git commit -m "$1"
     git push
     echo "Git repository updated. Now running update on server."
-    ssh lodetien@192.168.56.3 -p 44 "cd /var/www/html"
-	"sh update_server.sh"
+    ssh lodetien@192.168.56.3 -p 44 "sh update_server.sh"
     exit 0
 fi
 exit 1
